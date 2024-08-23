83a0a76e2f1229bc6ece8bb14bb484f6c1e4ecfb
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-08-23 12:32:31 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/main/java/com/mycompany/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_hola_mundo;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        //MOSTRAR UN SALUDO --> Documentar
+        System.out.println("HOLA MUNDO!!");
+    }
+}
