e0cda1760e132e1c5fbdd4cb89d6c86104ee0c8f
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-08-27 16:11:32 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/main/java/com/mycompany/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA1_2_VARIABLES {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+                                    //TIPOS DE DATOS EN JAVA
+      System.out.println("HOLA MUNDO!!"); // Cadena de texto
+      System.out.println(100);  //Un numero entero
+      System.out.println('c');   //Un caracter
+      System.out.println (true);    //Valores logicos
+      System.out.println(false);
+      System.out.println(1000.52); //Numeros con fraccion 
+      
+      //TIPOS DE DATOS
+      //ENTEROS
+      //int
+      //long
+      //short
+      //byte
+      //PUNTO FLOTANTE
+      //double
+      //float
+      //TEXTO
+      //String
+      //CARACTER
+      //char
+      //LÓGICOS (true false)
+      //boolean
+      
+      //creación y uso de variables
+     int edad; //declara una variable de tipo entero, llamada edad
+     //double edad; //NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE
+     String nombre; 
+     String apellido;
+     double promedio;
+     edad = 18;
+     
+     System.out.println(edad);
+     
+     //si se declara una variable se tiene que utilizar 
+      //DECLARACIÓN (tipo de dato y nombre de la variable)
+      // que operaciones debe o puede hacer
+      //cuanta memoria va a utilizar
+      //ASIGNACION --> poner un valor a la variable
+      
+    }
+}
