2382235c13fa8901c60fd7401939cc83eac6046d
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-09-02 16:09:46 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/main/java/com/mycompany/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+    //TODO code application logic here;
+    
+    String marca;
+    int pulgadas;
+    double resolucion;
+    double fluidez;
+    double precio;
+    
+    marca= "samsung";
+     pulgadas= 60;
+     resolucion= 1080;
+    fluidez= 120;
+    precio= 50000;
+   
+    
+    
+    System.out.println(marca);
+    System.out.println(pulgadas);
+    System.out.println(resolucion);
+    System.out.println(fluidez);
+    System.out.println(precio);
+           
+           }
+}
