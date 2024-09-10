b41280b594251790626c29973bcab905014597e7
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-09-10 16:48:37 -0600

    Add files via upload

---
+++ EVA1_13_AGUILA_SOL/src/main/java/com/mycompany/eva1_13_aguila_sol/EVA1_13_AGUILA_SOL.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_13_aguila_sol;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA1_13_AGUILA_SOL {
+
+    public static void main(String[] args) {
+       //Aleatorio
+       
+    double valor = Math.random();
+            //random genera valores >= 0 y <a 1
+            //entre 0 y .99999
+            if(valor < 0.5)
+               System.out.println("Aguila");
+     else 
+            System.out.println("Sello");
+
+
+                
+    }
+}
