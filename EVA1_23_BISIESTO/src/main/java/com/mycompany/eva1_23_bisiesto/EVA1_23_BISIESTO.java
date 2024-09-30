0c7088df1596bde9d68aa0ef6f60fc2273b4e970
Aram-villagran <L24550334@chihuahua2.tecnm.mx>
2024-09-30 16:43:30 -0600

    Add files via upload

---
+++ EVA1_23_BISIESTO/src/main/java/com/mycompany/eva1_23_bisiesto/EVA1_23_BISIESTO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_23_bisiesto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aramv
+ */
+public class EVA1_23_BISIESTO {
+
+    public static void main(String[] args) {
+        
+        //saber si es multiplo de 4
+        //residuo es 0 --> division exacta
+        //modulo --> calculo del residuo --> %
+        int modulo = 2000 % 4;
+        
+        modulo = 2000 % 100;
+        
+        modulo = 2000 % 400;
+      Scanner captu =  new Scanner (System.in);
+        
+    if (% 4 == modulo) 
+        
+        
+        
+        
+        
+    }
+}
